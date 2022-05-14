c2f7bf2f443fc2762366611ae717c7903fbadd0c
Aaron Zuspan <aa.zuspan@gmail.com>
2022-05-14 13:31:53 -0700

    Mocking pystac causes doc build to hang when registering dispatcher. Added it to doc requirements instead. (#8)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,4 +80,4 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 
-autodoc_mock_imports = ["numpy", "folium", "branca", "pystac"]
+autodoc_mock_imports = ["numpy", "folium", "branca"]
