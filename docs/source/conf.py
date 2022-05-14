e1d40caec7202411aa042499b9439c15fc5212ca
Aaron Zuspan <aa.zuspan@gmail.com>
2022-05-14 12:37:47 -0700

    Mock imports for RTD (#8)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,4 +80,4 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 
-autodoc_mock_imports = ["numpy", "folium", "branca"]
+autodoc_mock_imports = ["numpy", "folium", "branca", "pystac"]
