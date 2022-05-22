e4f3cbbaf5df2e78785823ff935d85f458a46e33
Aaron Zuspan <aa.zuspan@gmail.com>
2022-05-21 23:08:01 -0700

    Matplotlib is required for tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 requirements = ["pystac", "folium", "numpy", "branca"]
 doc_requirements = ["nbsphinx", "sphinx", "sphinx_rtd_theme"]
-test_requirements = ["pytest", "coverage"]
+test_requirements = ["pytest", "coverage", "matplotlib"]
 dev_requirements = (
     [
         "pre-commit",
