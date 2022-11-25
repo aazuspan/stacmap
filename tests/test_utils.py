9653df3b85768865ee244208f2cdd0dc3b454ce2
Aaron Zuspan <aa.zuspan@gmail.com>
2022-11-25 13:44:09 -0800

    Simplify import mocking

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
+from unittest import mock
+
 import pytest
 
 from stacmap import utils
 
-from .utils import mock_missing_import
-
 
 def test_cmap_branca():
     """Test that branca successfully retrieves a colorbrewer colormap"""
-    with mock_missing_import("matplotlib"):
+    with mock.patch.dict("sys.modules", {"matplotlib": None}):
         colors = utils.get_cmap("RdBu", 79)
         assert len(colors) == 79
 
@@ -20,6 +20,6 @@ def test_cmap_matplotlib():
 
 def test_cmap_matplotlib_missing():
     """Test that an error is raised if a non-colorbrewer colormap is requested without matplotlib"""
-    with mock_missing_import("matplotlib"):
+    with mock.patch.dict("sys.modules", {"matplotlib": None}):
         with pytest.raises(ValueError, match="pip install matplotlib"):
             utils.get_cmap("viridis", 18)
